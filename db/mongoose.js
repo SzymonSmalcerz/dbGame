9b73fcf0a5614c27397b30a4b6b896c76869b975
Szymon Smalcerz <smalcerzszymonn@gmail.com>
2017-10-26 19:29:05 +0200

    changed db

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/DragonBallDatabase', {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/DragonBallDatabase', {useMongoClient: true});
 
 module.exports = mongoose;
