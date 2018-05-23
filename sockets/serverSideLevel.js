1bb17b4edb6769250d3b2bad74389ac997f6815d
Szymon Smalcerz <smalcerzszymonn@gmail.com>
2018-05-23 11:31:47 +0200

    bug fixing vol 2

--- sockets/serverSideLevel.js
+++ sockets/serverSideLevel.js
@@ -205,10 +205,9 @@ class LevelFirst extends Level{
       var here = this;
       if(true){
         this.enemies[tempID] = new DarkKnight(tempID,x,y,this.players,this.enemies,this.statics,this.socketTable, function(){
-          here.numberOfHulks -= 1; 
+          here.numberOfHulks -= 1;
         });
-      }
-      }
+      };
 
       this.numberOfHulks += 1;
       for(var playerID in this.players){
